2de33521d2a76967cd17c4ae27ee1968326c6767
zhouqiao.zhao <zhouqiao.zhao@smartswarm.org>
2021-04-26 16:33:31 +0800

    use sprite to carousel in header

--- static/js/product.js
+++ static/js/product.js
@@ -1,4 +1,15 @@
 $(function(){
+    // 调整轮播图尺寸
+    let formatHeaderSize = ()=>{
+        let tag = document.querySelector(".carousel-inner .item.active")
+        if (!tag) return
+        let docs = document.querySelectorAll('.sprite')
+        for (let i=0; i < docs.length; i++) {
+            docs[i].style.zoom = Math.ceil( tag.clientWidth * 1000 / 1900) / 1000
+        }
+    }
+    window.onresize = formatHeaderSize
+    formatHeaderSize()
     $("#btn-close").click(()=>{
         const demo = document.getElementById("demo-iframe").contentWindow
         if (demo) demo.hide()
